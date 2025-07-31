3d4324b9325ac67d758cd6a21199c96c32793b16
zdr256 <88315648+zdr256@users.noreply.github.com>
2025-07-31 09:34:44 +0300

    Update and rename stores.html to stores.m

--- stores.m
+++ stores.m
@@ -1,3 +1,4 @@
+```html
 https://www.vali.bg/
 https://www.jarcomputers.com/
 https://gplay.bg/
@@ -8,4 +9,8 @@ https://www.olx.bg/
 https://bazar.bg/
 https://www.alo.bg/
 https://hop.bg/
+```
+
+```feedback
 https://docs.google.com/forms/d/e/1FAIpQLSflAQNt5eT7fV0X0uQgp8Q2UO4J0YiygGKtk_T__qJI1PZW4A/viewform?usp=preview
+```
